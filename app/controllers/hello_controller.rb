6d2bcc2f0f0c08f0eac38a96c7a9e60456bebcab
佐々木　莉奈 <1072sasaki@gmail.com>
2021-03-02 01:15:55 +0900

    Update RailsApp

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,9 @@
+class HelloController < ApplicationController
+  layout 'hello'
+  def index
+    @header = 'layout sample'
+    @footer = 'copyright SYODA-Tuyano 2016.'
+    @title = 'New Layout'
+    @msg = 'this is sample page!'
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'hello/index'
+  get 'hello', to: 'hello#index'
+  get 'helo/other'
+  post 'hello', to:'hello#index'
+  post 'hello/index'
+end
